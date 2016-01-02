938f37923519b5771c496c9534ff9f82a402a36a
Mukosame <mukosame@gmail.com>
2016-01-02 16:36:26 +0800

    add user::get_gender

--- zhihu.py
+++ zhihu.py
@@ -439,6 +439,26 @@ class User:
                 else:
                     return user_id
 
+    def get_gender(self):
+        """
+            By Mukosame (https://github.com/mukosame)
+            增加获取知乎识用户的性别
+             
+        """        
+        if self.user_url == None:
+            print "I'm anonymous user."
+            return 0
+        else:
+            if self.soup == None:
+                self.parser()
+            soup = self.soup
+            gender = str(soup.find("span",class_="item gender").i) 
+            if (gender == '<i class="icon icon-profile-female"></i>'):
+                return 'female'
+            else:
+                return 'male'
+
+
     def get_followees_num(self):
         if self.user_url == None:
             print "I'm anonymous user."
